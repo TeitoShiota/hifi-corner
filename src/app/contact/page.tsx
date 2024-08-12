445d53570a96fb0c0f6f6bea9d981366ed83e9b4
Teito Shiota <156904940+wu11-lasse-karahan-kristiansen@users.noreply.github.com>
2024-08-12 12:01:06 +0200

    chore:  add Sass dependency

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,10 @@
+export default function contact() {
+    return (
+        <main>
+            <h1>Contact</h1>
+            <p>
+                This is a simple Next.js app that demonstrates how to use the Next.js framework to build a web application.
+            </p>
+        </main>
+    );
+}
