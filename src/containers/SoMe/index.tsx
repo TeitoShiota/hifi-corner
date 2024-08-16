e62c707ff7211bd3b15d762bfe501783a728d341
wu11-ahu-karahan-kristiansen <u87327@rts-365.dk>
2024-08-16 14:14:50 +0200

    Added soMe and phone icons, modified and styled the footer.

--- src/containers/SoMe/index.tsx
+++ src/containers/SoMe/index.tsx
@@ -1,3 +1,13 @@
-export default function SoMe(props) {
-    return <a href={props.href}><img src={props.svg} /></a>
+import Image from "next/image"
+import Link from "next/link"
+
+export default function SoMe({href, svg, alt,} : {href: string, svg: string, alt: string}) {
+    return (
+        <Link href={`https://${href}`}>
+            <Image
+                src={svg}
+                alt={`Icon of ${alt}`}
+            />
+        </Link>
+    )
 }
