58dc8918dae300457cff312afb2d7ec18a2b739a
Ahu Karahan Kristiansen <u87327@rts-365.dk>
2024-08-19 19:46:16 +0200

    Added a hero video to the (home)page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,28 @@
-
 import '@/styles/homePage.scss';
 
 export default function Home() {
     return (
         <main className="main">
-            {/* Video */}
+
+            <video 
+                src="/hero_speakers.mp4" 
+                autoPlay 
+                // controls 
+                loop
+                className='heroVideo'
+            />
+
             {/* popular products */}
+            
             <section className="companyInfo">
                 <div className='companyDescription'>
                     <h3>What we do</h3>
                     <p>We look forward to customising a system to meet your needs.
-                    <br />
+                    <br /><br />
                     We don’t favour one manufacturer over another – the only thing we do favour is making sure our customers get the right product that suits their needs and listening preferences. We will ask many questions in order to ensure that what you buy from us is tailored to you and you alone.
-                    <br />
+                    <br /><br />
                     If you are looking for a product not found in our demonstration showrooms or our online site, don’t fret as we have access to hundreds of brands.
-                    <br />
+                    <br /><br />
                     One of our biggest pleasures of working in this industry is to see the smile on our customers’ faces when they finally hear and see the system of their dreams. </p>
                 </div>
                 <div className='openingHours'>
