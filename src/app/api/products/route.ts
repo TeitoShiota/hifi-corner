2b4648cc84c926cac3be3faa3a084e1cd2f84eee
Teito <lasselaflair@gmail.com>
2024-08-19 22:07:56 +0200

    Added comment for various functions

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -10,6 +10,18 @@ import { products } from './testData';
 
 export interface Request {
     // Define the properties of the request object here
+    // page: number | undefined;
+    
+    // limit: number | undefined;
+    // search: string | undefined;
+    // sort: string | undefined;
+    // order: string | undefined;
+    // category: string | undefined;
+    // color: string | undefined;
+    // size: string | undefined;
+    // price: string | undefined;
+    // stock: string | undefined;
+    // discount: string | undefined;
 }
 
 export interface Response {
@@ -18,8 +30,22 @@ export interface Response {
 
 
 
+// export async function GET(request: Request, res: Response): Promise<NextResponse> {
+//     return NextResponse.json(
+//         { products }
+//     );
+// }
+
+// GET function with query parameters for pagination
 export async function GET(request: Request, res: Response): Promise<NextResponse> {
+    const page = request.page || 1;
+    const limit = 10;
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+
+    const result = products.slice(startIndex, endIndex);
+
     return NextResponse.json(
-        { products }
+        { products: result }
     );
 }
--- src/lib/productsListLib.ts
+++ src/lib/productsListLib.ts
@@ -2,10 +2,31 @@ import { Product, ColorVariant } from '@/types/product';
 import { availableMemory } from 'process';
 
 // Type guards
+/**
+ * 
+ * @param product 
+ * @returns True if the product has colorVariants
+ * 
+ */
 function isProductWithVariants(product: Product): product is Product & { colorVariants: ColorVariant[] } {
     return (product as any).colorVariants !== undefined;
 }
 
+/**
+ * Returns the first image of the product or the first image of the first variant
+ * 
+ * @param product 
+ * @returns  The first image of the product or the first image of the first variant
+ * 
+ * @example
+ * const product = {
+ * images: ['image1', 'image2'],
+ * colorVariants: [
+ * {
+ * images: ['image3', 'image4']
+ * }]}
+ * 
+ */
 export function getProductImage(product: Product) {
     if (isProductWithVariants(product)) {
 
@@ -14,6 +35,22 @@ export function getProductImage(product: Product) {
     return product.images;
 }
 
+
+/**
+ * Returns the first price of the product or the first price of the first variant
+ * 
+ * @param product 
+ * @returns  The first price of the product or the first price of the first variant
+ * 
+ * @example
+ * const product = {
+ * price: 100,
+ * colorVariants: [
+ * {
+ * price: 200
+ * }]}
+ * 
+ */
 export function getProductPrice(product: Product) {
     if (isProductWithVariants(product)) {
 
@@ -24,7 +61,7 @@ export function getProductPrice(product: Product) {
 
 export function getProductStock(product: Product) {
     if (isProductWithVariants(product)) {
-        const stock = {
+        const stockInfo = {
             total: product.colorVariants.reduce((acc, variant) => acc + variant.stock, 0),
             availability: {
                 available: product.colorVariants.some(variant => variant.stock > 0),
