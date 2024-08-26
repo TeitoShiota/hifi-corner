db59bc5f5c65d4bdce63840a98ca71542cc916b3
Ahu Karahan Kristiansen <u87327@rts-365.dk>
2024-08-26 22:50:35 +0200

    Modified and styled the contact page.

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,10 +1,25 @@
+import '@/styles/subPage.scss';
+import Link from 'next/link';
+
 export default function contact() {
     return (
-        <main>
-            <h1>Contact</h1>
-            <p>
-                This is a simple Next.js app that demonstrates how to use the Next.js framework to build a web application.
-            </p>
+        <main className="pageMain">
+            <h1>GET IN TOUCH WITH US</h1>
+            <section>
+                <form action="" className='form'>
+                    <label>Full name <span className='orange'>*</span></label>
+                    <input type="text" className='inputField'/>
+                    <label>E-mail <span className='orange'>*</span></label>
+                    <input type="email" className='inputField'/>
+                    <label>Subject <span className='orange'>*</span></label>
+                    <input type="text" className='inputField'/>
+                    <label>Message <span className='orange'>*</span></label>
+                    <textarea className='inputMessage'></textarea>
+                    <button>Submit</button>
+                    {/* <input type="submit" value="Submit"></input> */}
+                </form>
+            </section>
+            <p className='affiliate-links'>Visit our sister companies <Link href="#" className='orange'>Home Sound</Link> and <Link href="#" className='orange'>The Movie Rooms</Link>part of the Hi-Fi Corner Group. </p>
         </main>
     );
 }
