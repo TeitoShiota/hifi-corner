3d7abbd5dc4570fa020b773997df701f910d6e90
Ahu Karahan Kristiansen <u87327@rts-365.dk>
2024-08-26 10:26:29 +0200

    ¨ - removed from the about page. :)

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -16,7 +16,7 @@ export default function About() {
                     alt={'History1'}
                     heading2="History"
                     heading3="Established in the late 1960s, our family owned business is based in Edinburgh and Falkirk, but services customers across the UK."
-                    text={`Our Edinburgh branch has the longest history as an audio retailer in the UK. During recent renovations, receipts were found from Nicolson’s Gramophone Saloon dating back to 1926. In the 1950s WG Graham took over the shop and renamed it WG Graham’s Hi-Fi Corner. Upon his retirement, Graham Tiso bought the business and shortened the name to Hi-Fi Corner.  ¨
+                    text={`Our Edinburgh branch has the longest history as an audio retailer in the UK. During recent renovations, receipts were found from Nicolson’s Gramophone Saloon dating back to 1926. In the 1950s WG Graham took over the shop and renamed it WG Graham’s Hi-Fi Corner. Upon his retirement, Graham Tiso bought the business and shortened the name to Hi-Fi Corner.
                         
                         Soon thereafter a young enthusiastic Colin MacKenzie (left), who was recommended by Linn’s own Ivor Tiefenbrun, was employed to manage the shop; with a knack for business and years of experience in the hi-fi industry, Colin would later become the owner of Hi-Fi Corner. Today, Struan
                         MacKenzie carries on the legacy as the company’s Managing Director.`}
