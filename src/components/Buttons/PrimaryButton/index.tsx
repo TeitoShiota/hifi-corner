43cdabbc079b8f13a9a31d7e28e9b92c866e53af
Teito Shiota <156904940+wu11-lasse-karahan-kristiansen@users.noreply.github.com>
2024-08-27 11:01:42 +0200

    chore: Update PrimaryButton component with disabled state support and commenting

--- src/components/Buttons/PrimaryButton/index.tsx
+++ src/components/Buttons/PrimaryButton/index.tsx
@@ -4,13 +4,41 @@ import './PrimaryButton.scss';
 
 interface PrimaryButtonProps {
     text: string;
-    // onClick: () => void;
+    onClick: () => void;
     className?: string;
+    disabled?: boolean;
 }
 
-export default function PrimaryButton({text, className}: PrimaryButtonProps) {
+/**
+ * @description - Button component with primary styling
+ * * Make sure to mark the parent component with 'use client' at the top of the file; to ensure the component is rendered on the client side
+ * 
+ * @param {string} text - Button text
+ * @param {string} className - Additional class names
+ * @param {function} onClick - Click event handler
+ * @param {boolean} disabled - Button disabled state
+ * @returns {JSX.Element} - Button component
+ * 
+ * @example
+ * <PrimaryButton
+ *      text="Contact Us"
+ *      onClick={() => alert('hello world')}
+ * />
+ * @example
+ * <PrimaryButton
+ *      text="Contact Us"
+ *      onClick={() => alert('hello world')}
+ *      className="contactButton"
+ *      disabled
+ * />
+ */
+export default function PrimaryButton({text, className, onClick, disabled = false}: PrimaryButtonProps) {
     return (
-        <button className={`primaryButton ${className}`}>
+        <button
+            className={`primaryButton ${className}`}
+            onClick={onClick}
+            disabled={disabled}
+        >
             {text}
         </button>
     )
