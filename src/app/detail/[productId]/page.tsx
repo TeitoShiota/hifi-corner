ae0619c0fa10ceb5d69d1d7f11f2517447db5523
Teito Shiota <156904940+wu11-lasse-karahan-kristiansen@users.noreply.github.com>
2024-08-28 11:23:52 +0200

    Chore: Implemented /detail API  endpoint and useFetchProductDetails hook.

--- src/app/detail/[productId]/page.tsx
+++ src/app/detail/[productId]/page.tsx
@@ -1,7 +1,14 @@
+'use client';
+import { useFetchProductDetails } from "@/hooks/useFetchProductDetails";
+
 export default function Detail({ params }: { params: { productId: string } }) {
     const { productId } = params;
 
-    
+    const { data, isLoading, isError } = useFetchProductDetails(productId);
+
+    if (isLoading) return <p>Loading...</p>;
+    if (isError) return <p>Error: {isError.message}</p>;
+
     return (
         <main>
             <h1>Detail</h1>
