d31734a9f7de96733d7c4892fbd779989873938e
Teito <lasselaflair@gmail.com>
2024-08-29 11:44:46 +0200

    Generated user data file

---
+++ src/data/userData.ts
@@ -0,0 +1,45 @@
+{
+    Users: [
+        {
+            id: 'f47ac10b-58cc-4372-a567-0e02b2c3d479',
+            name: 'John Doe',
+            email: 'john@doe.com',
+            password: '1223',
+            role: 'admin',
+            active: true
+        },
+        {
+            id: '9b2e3c4d-5f6a-4b7c-8d9e-0f1a2b3c4d5e',
+            name: 'Jane Doe',
+            email: 'jane_doe@doe.com',
+            password: '123',
+            role: 'user',
+            active: true
+        },
+        {
+            id: '1a2b3c4d-5e6f-7a8b-9c0d-1e2f3a4b5c6d',
+            name: 'John Smith',
+            email: 'smith@john.com',
+            password: '123',
+            role: 'user',
+            active: false
+        },
+        {
+            id: '2b3c4d5e-6f7a-8b9c-0d1e-2f3a4b5c6d7',
+            name: 'Teito Klein',
+            email: 'teito@teito.com',
+            password: '123',
+            role: 'user',
+            active: true
+        },
+        {
+            id: '3c4d5e6f-7a8b-9c0d-1e2f-3a4b5c6d7e8',
+            name: 'Johnny Doe',
+            email: 'doedoe@johnny.com',
+            password: '123',
+            role: 'user',
+            active: true
+        }
+    ]
+}
+
