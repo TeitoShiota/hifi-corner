8604ce383d8bc4e0b43bd982364af1d2af57f37d
Ahu Karahan Kristiansen <u87327@rts-365.dk>
2024-08-29 21:59:38 +0200

    Created the container and started the styling for Products Sort by.

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,10 +1,20 @@
 import ProductList from "@/containers/Products/ProductList";
+import ProductsSortBy from "@/containers/Products/ProductsSortBy";
+
+import '@/containers/Products/ProductsSortBy/products-sort-by.scss';
 
 export default function ProductsPage() {
     return (
         <main>
             <h1>Product</h1>
-            <ProductList />
+            <div className="products-page">
+                <section className="products-sort">
+                    <ProductsSortBy />
+                </section>
+                <section className="products-list">
+                    <ProductList />
+                </section>
+            </div>
         </main>
     );
 }
---
+++ src/containers/Products/ProductsSortBy/index.tsx
@@ -0,0 +1,61 @@
+'use client';
+
+export default function ProductsSortBy(){
+
+    return(
+        // <select>
+        //       <option value="someOption">Some option</option>
+        //       <option value="otherOption">Other option</option>
+        // </select>
+        <>
+            <h2>Sort by</h2>
+                <form action="">
+                    <h3>Brand</h3>
+                    <div>
+                        <label>Steelseries</label>
+                        <input type="radio" className='inputField'/>
+                    </div>
+                    <div>
+                        <label>Steelseries</label>
+                        <input type="radio" className='inputField'/>
+                    </div>
+                    <div>
+                        <label>Steelseries</label>
+                        <input type="radio" className='inputField'/>
+                    </div>
+                </form>
+                
+                <form action="">
+                    <h3>Brand</h3>
+                    <div>
+                        <label>Steelseries</label>
+                        <input type="radio" className='inputField'/>
+                    </div>
+                    <div>
+                        <label>Steelseries</label>
+                        <input type="radio" className='inputField'/>
+                    </div>
+                    <div>
+                        <label>Steelseries</label>
+                        <input type="radio" className='inputField'/>
+                    </div>
+                </form>
+                
+                <form action="">
+                    <h3>Brand</h3>
+                    <div>
+                        <label>Steelseries</label>
+                        <input type="radio" className='inputField'/>
+                    </div>
+                    <div>
+                        <label>Steelseries</label>
+                        <input type="radio" className='inputField'/>
+                    </div>
+                    <div>
+                        <label>Steelseries</label>
+                        <input type="radio" className='inputField'/>
+                    </div>
+                </form>
+        </>
+    )
+}
