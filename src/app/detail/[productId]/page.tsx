0043564abfb203047541c7f17543a06abf047aea
wu11-ahu-karahan-kristiansen <u87327@rts-365.dk>
2024-09-03 14:18:00 +0200

    Created, modified and styled the product details page and the Slideshow component/container.

--- src/app/detail/[productId]/page.tsx
+++ src/app/detail/[productId]/page.tsx
@@ -1,18 +1,83 @@
 'use client';
 import { useFetchProductDetails } from "@/hooks/useFetchProductDetails";
 
+
+import { getProductImage, getProductPrice, getProductStock } from "@/lib/productsListLib"
+
+import Slideshow from "@/containers/Slideshow";
+
+import './product-details.scss';
+
 export default function Detail({ params }: { params: { productId: string } }) {
     const { productId } = params;
 
     const { data, isLoading, isError } = useFetchProductDetails(productId);
+    console.log(data)
 
+    
+    
     if (isLoading) return <p>Loading...</p>;
     if (isError) return <p>Error: {isError.message}</p>;
+    
+    const productImage = getProductImage(data.product);
+
 
     return (
         <main>
-            <h1>Detail</h1>
-            <p>Product ID: {productId}</p>
+            <h1>PRODUCT</h1>
+            <section className="product-details-section">
+
+                <Slideshow images={productImage} />
+
+                <div>
+                    <h2>{data?.product.name}</h2>
+                    <p>{data?.product.description}</p>
+                </div>
+            </section>
+
+            <hr />
+
+            <section>
+                <h3>PRODUCT SPECIFICATIONS</h3>
+                <table>
+                    <tr>
+                        <td>Product ID</td>
+                        <td>{productId}</td>
+                    </tr>
+                    <tr>
+                        <td>Product ID</td>
+                        <td>{productId}</td>
+                    </tr>
+                    <tr>
+                        <td>Product ID</td>
+                        <td>{productId}</td>
+                    </tr>
+                    <tr>
+                        <td>Product ID</td>
+                        <td>{productId}</td>
+                    </tr>
+                    <tr>
+                        <td>Product ID</td>
+                        <td>{productId}</td>
+                    </tr>
+                    <tr>
+                        <td>Product ID</td>
+                        <td>{productId}</td>
+                    </tr>
+                    <tr>
+                        <td>Product ID</td>
+                        <td>{productId}</td>
+                    </tr>
+                    <tr>
+                        <td>Product ID</td>
+                        <td>{productId}</td>
+                    </tr>
+                    <tr>
+                        <td>Product ID</td>
+                        <td>{productId}</td>
+                    </tr>
+                </table>
+            </section>
         </main>
     );
 }
---
+++ src/containers/Slideshow/index.tsx
@@ -0,0 +1,65 @@
+import Image from 'next/image';
+import { useState } from 'react';
+
+import './slideshow.scss';
+
+export default function Slideshow( {images}: {images: string[] }) {
+    const [slideIndex, setSlideIndex] = useState(1); //usestate hook to manage slideIndex state
+
+    // Next/previous controls
+    const plusSlides = (n: number) => {
+        setSlideIndex((prevIndex) => {
+            const newIndex = prevIndex + n;
+            return newIndex > images.length ? 1 : newIndex < 1 ? images.length : newIndex;
+        });
+    };
+    
+    // Thumbnail image controls
+    const currentSlide = (n: number) => {
+        setSlideIndex(n);
+    };
+    
+    // Render slides
+    const renderSlides = () => {
+        return images.map((image, index) => ( //We're using the map function to render slides and dots.
+            <div
+                key={index}
+                className={`mySlides fade ${index === slideIndex - 1 ? 'active' : ''}`} //We're using template literals to conditionally apply the active class to slides and dots.
+            >
+                <Image 
+                    src={image} 
+                    alt="Product image" 
+                    width={300} 
+                    height={300} 
+                />
+            </div>
+        ));
+    };
+    
+    // Render dots
+    const renderDots = () => {
+        return images.map((_, index) => (
+            <span
+                key={index}
+                className={`dot ${index === slideIndex - 1 ? 'active' : ''}`}
+                onClick={() => currentSlide(index + 1)}
+            />
+        ));
+    };
+    
+
+    if (!images || images.length === 0) return null;
+
+    return (
+        <section>
+            <div className="slideshow-container">
+                <button className="prev" onClick={() => plusSlides(-1)}>&#10094;</button>
+                {renderSlides()}
+                <button className="next" onClick={() => plusSlides(1)}>&#10095;</button>
+            </div>
+            <div className="dots-container">
+                {renderDots()}
+            </div>
+        </section>
+    );
+}
